4282a6aba9931905acf5ec3b894bb6eb2103eade
rlfahs3025 <rlfahs3025@gmail.com>
2025-12-02 23:02:48 +0900

    Change name for github hosting

--- apply_update_japan_wordbook.sh
+++ apply_update_japan_wordbook.sh
@@ -1,2 +1,2 @@
-cp japan_wordbook_no_words.html japan_wordbook.html
-python word_init_script.py japan_wordbook.html results
+cp japan_wordbook_no_words.html index.html
+python word_init_script.py index.html results
